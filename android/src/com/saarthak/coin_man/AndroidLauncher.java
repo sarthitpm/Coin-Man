edbd5551ba738acfaae5247ccb613d22d03a896c
Saarthak Ambardekar <sarthi.ambardekar@gmail.com>
2020-12-30 19:51:36 +0530

    Initial Commit

---
+++ android/src/com/saarthak/coin_man/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.saarthak.coin_man;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.saarthak.coin_man.CoinMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinMan(), config);
+	}
+}
